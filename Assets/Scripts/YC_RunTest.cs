7e7fe3b506701b76f27b9194d6f5378f18b60f6c
yoonique1-admin <yooniquegame@gmail.com>
2024-11-22 17:48:31 +0900

    1. test 02 생성

---
+++ Assets/Scripts/YC_RunTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace InnerSight_YC
+{
+
+public class YC_RunTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+            Debug.Log("test2")
+        
+    }
+
+}
+
+}
